bf67300497c50750e49ed77afa8d7568ffb27b48
Karthik Babu <karthikjohnbabu@gmail.com>
2022-02-15 11:14:48 +0530

    Second Commit

--- Introduction/src/SelfIntroduction.java
+++ Introduction/src/SelfIntroduction.java
@@ -1,12 +1,18 @@
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SelfIntroduction {
 
 	public static void main(String[] args) {
-	//Invoking Browser
-	//Chrome - 
+		//Invoking Browser.
+		//Chrome- ChromeDriver-> Extends-> Methods like close and get
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.udemy.com/");
+		System.out.println(driver.getTitle());
+		driver.close();
 		
-		
-
 	}
 
 }
