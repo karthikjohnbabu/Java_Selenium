564b78f83044a8737cdbf7fa958e1c325db79839
Karthik Babu <karthikjohnbabu@gmail.com>
2022-02-25 10:14:29 +0530

    third commit

--- Introduction/src/SelfIntroduction.java
+++ Introduction/src/SelfIntroduction.java
@@ -8,11 +8,19 @@ public class SelfIntroduction {
 		//Invoking Browser.
 		//Chrome- ChromeDriver-> Extends-> Methods like close and get
 		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		//WebDriver methods + Class methods of chrome.
+		//The Driver object will have the knowledge of only the 
+		//Chrome driver implementation and not the personal class methods. 
+		
 		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://www.udemy.com/");
+		
 		System.out.println(driver.getTitle());
+		
 		driver.close();
 		
 	}
 
 }
+
