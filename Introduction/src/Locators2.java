e51f1e53a4d7732fdce36dabf89c358d98f8897d
Karthik Babu <karthikjohnbabu@gmail.com>
2022-03-30 00:17:21 +0530

    March 29th commit

--- Introduction/src/Locators2.java
+++ Introduction/src/Locators2.java
@@ -1,25 +1,45 @@
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.*;
 
 public class Locators2 {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
+		String name="Karthik"; 
 		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		WebDriver driver =new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password= getPassword(driver);
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.findElement(By.id("inputUsername")).sendKeys("Karthik");
-		driver.findElement(By.name("inputPassword")).sendKeys("EvanDiya");
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
-		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		Thread.sleep(1000);
+		System.out.println( driver.findElement(By.tagName("p")).getText());
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='login-container']/h2")).getText(),"Hello " + name+"," );
+		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+		driver.close();
+	}
+	public static String getPassword(WebDriver driver) throws InterruptedException
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.linkText("Forgot your password?")).click();
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("karthik");
+		driver.findElement(By.className("reset-pwd-btn")).click();
+		String passwordText=driver.findElement(By.cssSelector("form p")).getText();
+		String [] passwordArray = passwordText.split("'");
+		String [] passwordArray2 =passwordArray[1].split("'");
+		String password= passwordArray2[0];
+		return password;
+		//This split method returns an array. 0th Index -contains the left side of the array.
+		//1st Index -contains the right side of the array.
 		
 	}
 
---
+++ Introduction/src/sample.java
@@ -0,0 +1,6 @@
+public class sample{
+	public static void main (String[]args) {
+		System.setProperty("webdriver.drive.chrome", "chromedriver");
+		
+	}
+}
